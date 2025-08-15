b4bd1c3bf182898b7c14899ceb115a12e29a7aaa
Jerome D. Mangune <jeromemangune2004@gmail.com>
2025-08-15 16:27:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
+const portfolioSection = document.querySelector('#portfolio');
+const portfolioItems = portfolioSection.querySelectorAll('.portfolio-item, .heading');
+
+portfolioItems.forEach(item => {
+    item.classList.add('hidden-portfolio');
+});
+
+window.addEventListener('scroll', () => {
+    const rect = portfolioSection.getBoundingClientRect();
+    const screenMiddle = window.innerHeight / 2;
+
+    if (rect.top < screenMiddle && rect.bottom > screenMiddle) {
+        portfolioItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.classList.add('show-portfolio');
+            }, index * 500); // mabagal na delay
+        });
+    } else {
+        portfolioItems.forEach(item => {
+            item.classList.remove('show-portfolio');
+        });
+    }
+});
